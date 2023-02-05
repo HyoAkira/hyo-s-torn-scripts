eb28ed741617517b96624a3703cc84cede7b6a69
Umut Can Akçay <hyosuke@yandex.com>
2023-02-05 21:52:20 +0300

    Create item market list fast remover.js

---
+++ item market list fast remover.js
@@ -0,0 +1,9 @@
+//05.02.2023 - Hyō#1695
+let x =  document.getElementsByClassName("remove h-icon")
+ x = Array.from(x)
+
+x.forEach(x => x.click())
+let y =  document.getElementsByClassName("yes c-pointer")
+ y = Array.from(y)
+
+y.forEach(x => x.click())
